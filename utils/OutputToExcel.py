7922c6569ab3f3e82df274bc75b906fb8c78ff1f
muranhuli <muranhuli@qq.com>
2023-11-29 11:20:06 +0000

    计划输出时，红线欠修和红线过修日期写反的bug

--- utils/OutputToExcel.py
+++ utils/OutputToExcel.py
@@ -22,21 +22,21 @@ def output_wholeLife_plan(workpackage):
             interval_quarter = quarter_difference(date,last_mainten)
             over_under_repaired = int(work.Work_Package_Interval_Conversion_Value/90)-quarter_difference(date,last_mainten)
             over_under_repaired_percentage = over_under_repaired/(int(work.Work_Package_Interval_Conversion_Value/90))
-            # 红线欠修
-            red_line_repair_date = add_quarters(last_mainten,int(math.ceil(work.Work_Package_Interval_Conversion_Value*0.90/90)))
             # 红线过修
-            red_line_over_date = add_quarters(last_mainten,int(math.ceil(work.Work_Package_Interval_Conversion_Value*1.05/90)))
+            red_line_over_date = add_quarters(last_mainten,int(math.ceil(work.Work_Package_Interval_Conversion_Value*0.90/90)))
+            # 红线欠修
+            red_line_repair_date = add_quarters(last_mainten,int(math.ceil(work.Work_Package_Interval_Conversion_Value*1.05/90)))
             track_type = ''
             tmp = [work.Train_Type,work.Train_Number,work.Work_Package_Number,work.Work_Package_Name,work.Electrifiable,work.Powerless,work.Work_Package_Interval_Time_Dimension,
                    work.Work_Package_Interval_Value,work.Work_Package_Interval_Conversion_Value,work.Work_Package_Person_Day,work.Work_Package_Person_Day_Unit_Price,work.Need_Trial_Run,
                    work.Work_On_Non_Working_Day,work.Cooling_Time,work.Shared_Cooling_Work_Package_Number,work.Sampling_Times,work.Combined_Work_Package_Number,work.Need_High_Voltage_Verification,
-                   work.Work_Package_Contract,work.Undercarriage_Assembly,date,last_mainten,interval_quarter,over_under_repaired,over_under_repaired_percentage,red_line_repair_date,red_line_over_date,track_type]
+                   work.Work_Package_Contract,work.Undercarriage_Assembly,date,last_mainten,interval_quarter,over_under_repaired,over_under_repaired_percentage,red_line_over_date,red_line_repair_date,track_type]
             mainten_quarter.append(tmp)
     
     mainten_quarter.sort(key=lambda x: (x[20], x[8], x[1], x[2],x[21],x[13]), reverse=False)
     columns_name = ['列车类型','列车编号','工作包编号','工作包名称','是否可以通电工作','是否可以断电工作','工作包间隔时间维度','工作包间隔值','工作包间隔转换值', '工作包人天', '工作包人天单价',
                '是否需要试车','是否在工作日工作','冷却时间(天)','共享冷却工作包编号','抽检次数','组合工作包编号','是否需要高压验证','工作包承包','是否车下拆装','本次维修时间','上次维修时间',
-               '间隔季度差值','过修季度','过修百分比','红线欠修日期','红线过修日期','股道类型']
+               '间隔季度差值','过修季度','过修百分比','红线过修日期','红线欠修日期','股道类型']
     
     df = pd.DataFrame(columns=columns_name, data=mainten_quarter)
     folder_path = './results'
@@ -74,24 +74,24 @@ def output_year_plan(workpackage):
             interval_quarter = month_difference(date,last_mainten)
             over_under_repaired = int(work.Work_Package_Interval_Conversion_Value/30)-month_difference(date,last_mainten)- int(int(work.Work_Package_Interval_Conversion_Value)/2190)                                                                                                       
             over_under_repaired_percentage = over_under_repaired/(int(work.Work_Package_Interval_Conversion_Value/30))
-            # 红线欠修
-            if work.Work_Package_Interval_Conversion_Value>90:
-                red_line_repair_date = add_months(last_mainten,int(math.ceil(work.Work_Package_Interval_Conversion_Value*0.90/90)))
-            else:
-                red_line_repair_date = add_months(last_mainten,int(math.ceil(work.Work_Package_Interval_Conversion_Value*0.95/30)))
             # 红线过修
-            red_line_over_date = add_months(last_mainten,int(math.ceil(work.Work_Package_Interval_Conversion_Value*1.05/90)))
+            if work.Work_Package_Interval_Conversion_Value>90:
+                red_line_over_date = add_months(last_mainten,int(math.ceil(work.Work_Package_Interval_Conversion_Value*0.90/90)))
+            else:
+                red_line_over_date = add_months(last_mainten,int(math.ceil(work.Work_Package_Interval_Conversion_Value*0.95/30)))
+            # 红线欠修
+            red_line_repair_date = add_months(last_mainten,int(math.ceil(work.Work_Package_Interval_Conversion_Value*1.05/90)))
             track_type = ''                
                 
             tmp = [work.Train_Type,work.Train_Number,work.Work_Package_Number,work.Work_Package_Name,work.Electrifiable,work.Powerless,work.Work_Package_Interval_Time_Dimension,
                    work.Work_Package_Interval_Value,work.Work_Package_Interval_Conversion_Value,work.Work_Package_Person_Day,work.Work_Package_Person_Day_Unit_Price,work.Need_Trial_Run,
                    work.Work_On_Non_Working_Day,work.Cooling_Time,work.Shared_Cooling_Work_Package_Number,work.Sampling_Times,work.Combined_Work_Package_Number,work.Need_High_Voltage_Verification,
-                   work.Work_Package_Contract,work.Undercarriage_Assembly,date,last_mainten,interval_quarter,over_under_repaired,over_under_repaired_percentage,red_line_repair_date,red_line_over_date,track_type]
+                   work.Work_Package_Contract,work.Undercarriage_Assembly,date,last_mainten,interval_quarter,over_under_repaired,over_under_repaired_percentage,red_line_over_date,red_line_repair_date,track_type]
             mainten_month.append(tmp)
     mainten_month.sort(key=lambda x: (x[20], x[8], x[1], x[2],x[21],x[13]), reverse=False)
     columns_name = ['列车类型','列车编号','工作包编号','工作包名称','是否可以通电工作','是否可以断电工作','工作包间隔时间维度','工作包间隔值','工作包间隔转换值', '工作包人天', '工作包人天单价',
                '是否需要试车','是否在工作日工作','冷却时间(天)','共享冷却工作包编号','抽检次数','组合工作包编号','是否需要高压验证','工作包承包','是否车下拆装','本次维修时间','上次维修时间',
-               '间隔月份差值','过修月份','过修百分比','红线欠修日期','红线过修日期','股道类型']
+               '间隔月份差值','过修月份','过修百分比','红线过修日期','红线欠修日期','股道类型']
     
     df = pd.DataFrame(columns=columns_name, data=mainten_month)
     folder_path = './results'
@@ -130,24 +130,24 @@ def output_month_plan(workpackage):
             interval_quarter = (date-last_mainten).days
             over_under_repaired = int(work.Work_Package_Interval_Conversion_Value)-(date-last_mainten).days
             over_under_repaired_percentage = over_under_repaired/int(work.Work_Package_Interval_Conversion_Value)
-            # 红线欠修
-            if work.Work_Package_Interval_Conversion_Value>90:
-                red_line_repair_date = last_mainten + relativedelta(days=int(work.Work_Package_Interval_Conversion_Value*0.90))
-            else:
-                red_line_repair_date = last_mainten + relativedelta(days=int(work.Work_Package_Interval_Conversion_Value*0.95))
             # 红线过修
-            red_line_over_date = last_mainten + relativedelta(days=int(work.Work_Package_Interval_Conversion_Value*1.05))
+            if work.Work_Package_Interval_Conversion_Value>90:
+                red_line_over_date = last_mainten + relativedelta(days=int(work.Work_Package_Interval_Conversion_Value*0.90))
+            else:
+                red_line_over_date = last_mainten + relativedelta(days=int(work.Work_Package_Interval_Conversion_Value*0.95))
+            # 红线欠修
+            red_line_repair_date = last_mainten + relativedelta(days=int(work.Work_Package_Interval_Conversion_Value*1.05))
             track_type = ''                
                 
             tmp = [work.Train_Type,work.Train_Number,work.Work_Package_Number,work.Work_Package_Name,work.Electrifiable,work.Powerless,work.Work_Package_Interval_Time_Dimension,
                    work.Work_Package_Interval_Value,work.Work_Package_Interval_Conversion_Value,work.Work_Package_Person_Day,work.Work_Package_Person_Day_Unit_Price,work.Need_Trial_Run,
                    work.Work_On_Non_Working_Day,work.Cooling_Time,work.Shared_Cooling_Work_Package_Number,work.Sampling_Times,work.Combined_Work_Package_Number,work.Need_High_Voltage_Verification,
-                   work.Work_Package_Contract,work.Undercarriage_Assembly,date.strftime("%Y-%m-%d"),last_mainten.strftime("%Y-%m-%d"),interval_quarter,over_under_repaired,over_under_repaired_percentage,red_line_repair_date.strftime("%Y-%m-%d"),red_line_over_date.strftime("%Y-%m-%d"),track_type]    
+                   work.Work_Package_Contract,work.Undercarriage_Assembly,date.strftime("%Y-%m-%d"),last_mainten.strftime("%Y-%m-%d"),interval_quarter,over_under_repaired,over_under_repaired_percentage,red_line_over_date.strftime("%Y-%m-%d"),red_line_repair_date.strftime("%Y-%m-%d"),track_type]    
             mainten_day.append(tmp)
     mainten_day.sort(key=lambda x: (x[20], x[8], x[1], x[2],x[21],x[13]), reverse=False)        
     columns_name = ['列车类型','列车编号','工作包编号','工作包名称','是否可以通电工作','是否可以断电工作','工作包间隔时间维度','工作包间隔值','工作包间隔转换值', '工作包人天', '工作包人天单价',
                '是否需要试车','是否在工作日工作','冷却时间(天)','共享冷却工作包编号','抽检次数','组合工作包编号','是否需要高压验证','工作包承包','是否车下拆装','本次维修时间','上次维修时间',
-               '间隔天数差值','过修天数','过修百分比','红线欠修日期','红线过修日期','股道类型']
+               '间隔天数差值','过修天数','过修百分比','红线过修日期','红线欠修日期','股道类型']
     
     col = -1
     for index, info in enumerate(mainten_day):
